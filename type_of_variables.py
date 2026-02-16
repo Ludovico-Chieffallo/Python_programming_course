f015206182ac212673710578515a2dc30d142f80
Ludovico-Chieffallo <ludovico.chieffallo@outlook.it>
2026-02-16 18:22:26 +0100

    fixed

--- type_of_variables.py
+++ type_of_variables.py
@@ -1,3 +1,21 @@
 #Integer
 x = 2
 print(type(x))
+
+#float/double
+
+y = 2.5
+print(type(y))
+
+#string
+
+a = "hello"
+print(type(a))
+
+#logical
+
+q1 = True
+q2 = False
+
+print(type(q1))
+print(type(q2))
